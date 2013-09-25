e8d1c71775d5c43e5bee0b4cd66a9af434d7f7e6
Baseev Kumar <baseev@gmail.com>
2013-09-25 09:54:07 +0530

    refactoring

--- main/views.py
+++ main/views.py
@@ -5,8 +5,6 @@ from django.contrib.auth.decorators import login_required
 
 @login_required
 def Profile(request):
-    if not request.user.is_authenticated():
-        return HttpResponseRedirect('/accounts/login/')
     profile    = request.user
     context = {'profile' : profile, 'a' : 'b'}     
     return render_to_response('profile.html', context, context_instance=RequestContext(request)) 
